50fa54fbdfdb7255a90a01be4962e0572a604e7c
Weber Jaroslaw <weber-jaroslaw@dmm.com>
2020-06-22 14:24:19 +0900

    somehow working

--- src/PathFinder.cpp
+++ src/PathFinder.cpp
@@ -1,8 +1,11 @@
 
 #include <vector>
+#include <iostream>
 using std::vector;
 
-class Position
+using std::cout;
+
+struct Position
 {
 public:
 	int x;
@@ -37,10 +40,40 @@ class Map
 {
 public:
 	vector<Node> nodes;
-	unsigned char *grid;
 
 	int width;
 	int height;
+    vector<bool> grid;
+
+	void Constructor(vector<bool> _grid, int _width, int _height)
+	{
+		width = _width;
+		height = _height;
+		grid = _grid;
+
+		nodes = vector<Node>();
+        int gridSize = std::size(_grid);
+		for (size_t i = 0; i < gridSize; i++)
+		{
+            //cout << "i"<<i;
+			/* code */
+            int x = i/width;
+            int y = i%width;
+            Node n = Node();
+            Position p = Position();
+            p.x = x;
+            p.y = y;
+            n.position = p;
+            nodes.push_back(n);
+		}
+
+		//nodes.push_back(node);
+	}
+    
+    void Print()
+    {
+        
+    }
 
 	bool IsTraversable(Position position)
 	{
@@ -57,12 +90,12 @@ class PathFinder
 {
 	unsigned char *map;
 
-	Node opened;
-	Node closed;
+	vector<Position> opened;
+	vector<Position> closed;
 
 	int FindPath(const int nStartX, const int nStartY,
 				 const int nTargetX, const int nTargetY,
-				 const unsigned char *pMap, const int nMapWidth, const int nMapHeight,
+				 const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
 				 int *pOutBuffer, const int nOutBufferSize)
 	{
 		//todo
@@ -77,5 +110,14 @@ class PathFinder
 
 int main()
 {
+    Map m;
+    //const unsigned char cmd1[] = {0x01, 0x00};
+
+    //const unsigned char* mc2 = cmd1;
+    vector<bool> v;
+    v.push_back(true);
+    v.push_back(true);
+    v.push_back(false);
+    m.Constructor(v, 3,1);
 	return 0;
 }
