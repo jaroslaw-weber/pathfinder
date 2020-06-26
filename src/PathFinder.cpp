f0bfae98421a4fe1a988eb0dceb83bf87bf5b763
Weber Jaroslaw <weber-jaroslaw@dmm.com>
2020-06-26 11:52:52 +0900

    checkpoint - working

--- src/PathFinder.cpp
+++ src/PathFinder.cpp
@@ -73,7 +73,7 @@ public:
 		}
 		else if (canTraverse)
 		{
-			return " ";
+			return ".";
 		}
 
 		return "X";
@@ -110,6 +110,61 @@ public:
 	int height;
 
 	vector<bool> grid;
+};
+
+class PathPrinter
+{
+public:
+	vector<shared_ptr<Node>> nodes;
+	/*
+printing nodes.
+
+it will look like this:
+__________
+....
+X.X.
+X...
+__________
+
+it can also print path like this:
+
+__________
+p...
+XpX.
+Xp..
+__________
+
+symbols:
+X -> cannot traverse
+. -> can traverse 
+p -> path
+*/
+	void Print()
+	{
+		cout << "\n__________\n";
+		int count = std::size(nodes);
+		int currentY = 0;
+		for (size_t i = 0; i < count; i++)
+		{
+			shared_ptr<Node> node = nodes.at(i);
+			int y = node->position.y;
+			if (currentY != y)
+			{
+				cout << "\n";
+				currentY = y;
+			}
+			cout << node->ToLetter();
+		}
+		cout << "\n__________\n";
+	}
+
+	void PrintEachNodeInfo()
+	{
+		for_each(nodes.begin(), nodes.end(), [](shared_ptr<Node> &n) {
+			cout << "\n";
+			cout << n->ToString();
+		});
+	}
 };
 
 //finding path using A* algorithm
@@ -169,8 +224,11 @@ public:
 			bool current = pMap[i] == 1;
 			grid.push_back(current);
 		}
-		FindPath(start, end, m);
-		return 0;
+		m.grid = grid;
+		auto path = FindPath(start, end, m);
+		//how many steps
+		int stepCount = std::size(path) - 1;
+		return stepCount;
 	}
 
 	shared_ptr<Node> GetStartNode()
@@ -188,12 +246,20 @@ public:
 	//find path (simplified function signature)
 	vector<Position> FindPath(Position start, Position end, Map map)
 	{
+
 		m_start = start;
 		m_end = end;
 		m_map = map;
 		m_opened = vector<shared_ptr<Node>>();
 		m_closed = vector<shared_ptr<Node>>();
 		CreateNodes();
+
+		//print map
+		cout << "\nmap:";
+		PathPrinter pp;
+		pp.nodes = m_nodes;
+		pp.Print();
+		//
 
 		shared_ptr<Node> current = GetStartNode();
 		m_opened.push_back(current);
@@ -246,6 +312,8 @@ public:
 		for_each(path.begin(), path.end(), [this](Position &p) {
 			this->GetNode(p).value()->isSpecial = true;
 		});
+		cout << "\npath:";
+		pp.Print();
 		return path;
 	}
 
@@ -283,31 +351,36 @@ private:
 		//cout << "\nget neighbor: " << position.ToString();
 		vector<shared_ptr<Node>> arr;
 
-		for (int i = position.x; i < position.x + 2; i++)
-		{
-			for (int j = position.y - 1; j < position.y + 2; j++)
+		Position p1;
+		Position p2;
+		Position p3;
+		Position p4;
+		p1.x = position.x - 1;
+		p2.x = position.x + 1;
+		p1.y = position.y;
+		p2.y = position.y;
+		p3.x = position.x;
+		p4.x = position.x;
+		p3.y = position.y - 1;
+		p4.y = position.y + 1;
+		auto positions = vector<Position>();
+		positions.push_back(p1);
+		positions.push_back(p2);
+		positions.push_back(p3);
+		positions.push_back(p4);
+
+		for_each(positions.begin(), positions.end(), [this, &arr](Position &p) {
+			auto neighbor = this->GetNode(p);
+			if (neighbor.has_value())
 			{
-
-				Position p;
-				p.x = i;
-				p.y = j;
-
-				if (p.IsEqual(position))
+				auto neighborNode = neighbor.value();
+				//can it be traversed? if no then ignore
+				if (neighborNode->canTraverse)
 				{
-					continue;
-				}
-				auto neighbor = GetNode(p);
-				if (neighbor.has_value())
-				{
-					auto neighborNode = neighbor.value();
-					//can it be traversed? if no then ignore
-					if (neighborNode->canTraverse)
-					{
-						arr.push_back(neighborNode);
-					}
+					arr.push_back(neighborNode);
 				}
 			}
-		}
+		});
 
 		return arr;
 	}
@@ -377,40 +450,9 @@ private:
 	}
 };
 
-class PathPrinter
-{
-public:
-	vector<shared_ptr<Node>> nodes;
-
-	void Print()
-	{
-		cout << "\n";
-		int count = std::size(nodes);
-		int currentY = 0;
-		for (size_t i = 0; i < count; i++)
-		{
-			shared_ptr<Node> node = nodes.at(i);
-			int y = node->position.y;
-			if (currentY != y)
-			{
-				cout << "\n";
-				currentY = y;
-			}
-			cout << node->ToLetter();
-		}
-	}
-
-	void PrintEachNodeInfo()
-	{
-		for_each(nodes.begin(), nodes.end(), [](shared_ptr<Node> &n) {
-			cout << "\n";
-			cout << n->ToString();
-		});
-	}
-};
-
 void exampleTest()
 {
+	cout << "\n===example1===\n";
 	unsigned char pMap[] = {1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1};
 	int pOutBuffer[12];
 	PathFinder pf;
@@ -419,7 +461,7 @@ void exampleTest()
 
 void myTest()
 {
-
+	cout << "=== custom test ===";
 	Map m;
 	//const unsigned char cmd1[] = {0x01, 0x00};
 
@@ -429,7 +471,7 @@ void myTest()
 	{
 		v.push_back(true);
 		v.push_back(true);
-		v.push_back(false);
+		v.push_back(true);
 		v.push_back(true);
 		v.push_back(false);
 		v.push_back(true);
@@ -441,13 +483,10 @@ void myTest()
 	PathFinder pf;
 	Position p1;
 	Position p2;
-	p2.x = 22;
-	p2.y = 7;
+	p2.x = 28;
+	p2.y = 3;
 
 	auto path = pf.FindPath(p1, p2, m);
-	PathPrinter pp;
-	pp.nodes = pf.m_nodes;
-	pp.Print();
 };
 
 int main()
